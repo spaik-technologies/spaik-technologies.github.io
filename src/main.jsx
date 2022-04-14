9fd4b3f32d15fe671efff0475000c44ea156f17b
ejar <ejarkm@gmail.com>
2022-04-14 20:30:41 +0200

    deploynment

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
-     <App />
-   </Router>,
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 )
