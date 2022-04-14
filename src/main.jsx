ed52061d5e7d0c625ec65c470a8e960eea3a26df
ejar <ejarkm@gmail.com>
2022-04-14 21:24:17 +0200

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.render(
-  <Router base="/">
-    <App />
-  </Router>,
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>,
   document.getElementById('root')
 )
