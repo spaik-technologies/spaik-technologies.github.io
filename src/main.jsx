e8c10c313f6d966ca894347692dbd9befa63c9f6
ejar <ejarkm@gmail.com>
2022-04-14 21:32:00 +0200

    dep

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import { HashRouter as Router } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.render(
+  <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
       <App />
-    </Router>,
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 )
