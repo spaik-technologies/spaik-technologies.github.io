7ec99652e9b57ef029c60abfb1a9b891a24d0417
ejar <ejarkm@gmail.com>
2022-04-15 00:48:08 +0200

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import App from './App'
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
-import Home from './pages/Home';
 
-import './css/style.scss';
-
-import AOS from 'aos';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       <App />
     </Router>
   </React.StrictMode>,
