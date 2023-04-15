0e86f4f5569b9d6e58b9c023fff2f146e7c1be60
ejar <ejarkm@gmail.com>
2023-04-15 12:54:37 +0200

    Updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { FooterContainer, FooterWrap, FooterLinkTitle } from "./FooterElements";
+import { MDBIcon } from "mdbreact";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = ({ id }) => {
   return (
     <FooterContainer id={id}>
       <FooterWrap>
         <FooterLinkTitle>Contact us</FooterLinkTitle>
-        <b>spaik.technologies@gmail.com</b>
+        <b><MDBIcon color="black" icon="envelope"/> spaik.technologies@gmail.com</b>
         <p>&nbsp;</p>
         <p>© Spaik Technologies S.L.</p>
         <p>All Rights Reserved</p>
